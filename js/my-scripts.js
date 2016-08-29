bf32f815ef2865659f5aed2f4f29c3854b6153e3
GuidoPettinari <guidopettinari@hotmail.com>
2016-08-29 03:30:11 -0300

    Primera subida Huerta

---
+++ js/my-scripts.js
@@ -0,0 +1,21 @@
+(function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(1500)
+            .delay(1500)
+            .fadeOut(1500, showNextQuote);
+    }
+
+    showNextQuote();
+
+})();
+
+
+$('.carousel').carousel({
+  interval: 2500
+})
